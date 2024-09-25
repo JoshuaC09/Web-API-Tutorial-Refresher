dd238835fb8a4bf9b8b56902d9f5214ae54cbf3a
JoshuaC09 <117239417+JoshuaC09@users.noreply.github.com>
2024-09-26 00:10:01 +0800

    Revert "Built in logger and log levels and serilog to file"

--- CollegeApp/CollegeApp/obj/Debug/net8.0/CollegeApp.AssemblyInfo.cs
+++ CollegeApp/CollegeApp/obj/Debug/net8.0/CollegeApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollegeApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f9d8a76f0b71798ae044390dd0df0a1cf488594")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollegeApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollegeApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
