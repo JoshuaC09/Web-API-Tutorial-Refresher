cc064bd2299c863e03a65ae9deff1a3b6cc8e697
JoshuaC09 <117239417+JoshuaC09@users.noreply.github.com>
2024-09-25 13:24:14 +0800

    Initial Commit

---
+++ CollegeApp/CollegeApp/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using CollegeApp.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController:ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Student> GetStudentName()
+        {
+            return CollegeRepository.Students;
+        }
+
+        [HttpGet ("{id:int}")]
+        public Student GetStudent(int id)
+        {
+            return CollegeRepository.Students.Where(item => item.Id == id).FirstOrDefault();
+        }
+    }
+}
---
+++ CollegeApp/CollegeApp/Model/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace CollegeApp.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }   
+        public int Allowance { get; set; }
+    }
+}
