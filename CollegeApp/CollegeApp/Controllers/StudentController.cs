a61efc1623e0b29b3e005867e3d60f541b797b4c
JoshuaC09 <117239417+JoshuaC09@users.noreply.github.com>
2024-09-25 15:13:28 +0800

    Added API response documentation

--- CollegeApp/CollegeApp/Controllers/StudentController.cs
+++ CollegeApp/CollegeApp/Controllers/StudentController.cs
@@ -8,15 +8,53 @@ namespace CollegeApp.Controllers
     public class StudentController:ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Student> GetStudentName()
+        [Route("All",Name = "GetAllStudent")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+      
+        public ActionResult<IEnumerable<Student>> GetStudentName()
         {
-            return CollegeRepository.Students;
+            return Ok(CollegeRepository.Students);
         }
 
-        [HttpGet ("{id:int}")]
-        public Student GetStudent(int id)
+        [HttpGet ("{id:int}",Name ="GetStudentById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public ActionResult<Student> GetStudent(int id)
         {
-            return CollegeRepository.Students.Where(item => item.Id == id).FirstOrDefault();
+
+            if(id <= 0)
+            {
+                return BadRequest("Invalid Id");
+            }
+            Student student = CollegeRepository.Students.Where(item => item.Id == id).FirstOrDefault();
+            if (student == null)
+            {
+                return NotFound($"Student {id} not exist int the database");
+            }
+            return Ok(student);
+        }
+
+        [HttpDelete]
+        [Route("{id:int:min(1):max(100)}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+
+        public ActionResult<bool> DeleteStudent(int id)
+        {
+
+            Student student = CollegeRepository.Students.Where(student => student.Id == id).FirstOrDefault();
+
+            if(student == null)
+            {
+                return NotFound($"Student with {id} is not found");
+            }
+            CollegeRepository.Students.Remove(student);
+
+            return Ok(true);
         }
     }
 }
